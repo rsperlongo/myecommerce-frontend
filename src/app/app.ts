2eb9144dd880bde73a7762e5bd3c7c418608253a
Ricardo Tadeu Sperlongo <ricardotadeusperlongo@MacBook-Air-de-Ricardo.local>
2026-03-10 09:01:24 -0300

    added header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Home } from './home/home';
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',

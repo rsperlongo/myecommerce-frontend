18db295406f272c77ecd5df2055e2be5c6fc14b2
Ricardo Tadeu Sperlongo <ricardotadeusperlongo@MacBook-Air-de-Ricardo.local>
2026-03-10 14:33:45 -0300

    added scss styles in header

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatGridList, MatGridListModule } from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "../components/header/header";
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, Header],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })

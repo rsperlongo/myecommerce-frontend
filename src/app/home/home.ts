d4f5299446e1986b7f0de1e0557dfb08c7b0dde1
Ricardo Tadeu Sperlongo <ricardotadeusperlongo@MacBook-Air-de-Ricardo.local>
2026-03-16 16:16:46 -0300

    create collumns and product example

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { Header } from "../components/header/header";
 import { Footer } from "../components/footer/footer";
 import { Sidenav } from "../components/sidenav/sidenav";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, Header, Footer, Sidenav],
+  imports: [MatGridListModule, MatCardModule, MatButtonModule, Header, Footer, Sidenav],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {}
